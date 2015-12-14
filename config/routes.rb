326f59bf1186b1ed13070e3baee66de4f3ae5f87
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-12-14 15:06:50 -0800

    adds lists controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   mount_griddler
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :lists, :posts
 
   # Example resource route with options:
   #   resources :products do
