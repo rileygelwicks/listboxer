2f823aa3ba3412e750bcf2cdc5f914dcd563986b
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-12-14 23:00:16 -0800

    creates new list if email from doesnt exist in databse

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,25 +4,26 @@ class EmailProcessor
   end
 
   def process
-    p @email.from[:email]
-    p @email.subject
-    # p @email.raw_html
-    # testJson = {"html" => @email.raw_html}
-    # testJson.to_json
-    # p JSON.parse([@email.raw_html].to_json).first
-    @email.raw_html.gsub!(/\r\n/,'').gsub!(/\t/, '')
-    @email.raw_html.gsub!(/[\\"]/,'')
-    p @email.raw_html
-    # JSON.parse([ 'foo' ].to_json).first
-
-    # all of your application-specific code here - creating models,
-    # processing reports, etc
-
-    # here's an example of model creation
-    # user = User.find_by_email(@email.from[:email])
-    # user.posts.create!(
-    #   subject: @email.subject,
-    #   body: @email.body
-    # )
+    if List.exists?(fromAddress: @email.from[:email])
+      list = List.find_by fromAddress:(@email.from[:email])
+      post_create(list, @email)
+    else
+      List.create!(
+        fromAddress: @email.from[:email],
+      )
+      list = List.find_by fromAddress:(@email.from[:email])
+      post_create(list, @email)
+    end
+  end
+  
+  def post_create(list,email)
+    email.raw_html.gsub!(/\r\n/,'').gsub!(/\t/, '')
+    email.raw_html.gsub!(/[\\"]/,'')
+    list.posts.create!(
+      subject: email.subject,
+      body: email.raw_html,
+      from_email: email.from[:email],
+      to_email: email.to.first[:email]
+    )
   end
 end
