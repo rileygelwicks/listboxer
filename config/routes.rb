21695d44e28a53e84c953dd107b6a5e5c024b428
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-12-14 23:07:50 -0800

    changes root to list#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'lists#index'
   mount_griddler
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :lists, :posts
