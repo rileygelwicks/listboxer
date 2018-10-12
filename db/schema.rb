abb63a4a416c9d5463fa56a0de369fff974f1858
Riley Gelwicks <riley.gelwicks@gmail.com>
2018-10-12 16:14:38 -0700

    Updates rails

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -23,10 +22,9 @@ ActiveRecord::Schema.define(version: 20151217054908) do
     t.integer  "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_comments_on_list_id", using: :btree
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
   end
-
-  add_index "comments", ["list_id"], name: "index_comments_on_list_id", using: :btree
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
