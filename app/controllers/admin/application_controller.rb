fa3f8773fde4e60887ae4fbf1e686e7cb1b571c3
Riley Gelwicks <riley.gelwicks@gmail.com>
2018-10-14 11:40:36 -0700

    adds auth to admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,7 +9,10 @@ module Admin
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      http_basic_authenticate_with(
+        name: ENV.fetch("ADMIN_NAME"),
+        password: ENV.fetch("ADMIN_PASSWORD")
+      ) unless Rails.env == 'test' || 'development'
     end
 
     # Override this value to specify the number of elements to display at a time
